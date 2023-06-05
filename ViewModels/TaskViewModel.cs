2cb872e95edaf515f8f6b58888e8483763e0a225
NSRJyothishchandra <narayana.jyothish@gmail.com>
2023-06-04 21:53:01 -0500

    implementation of i notify property change

--- ViewModels/TaskViewModel.cs
+++ ViewModels/TaskViewModel.cs
@@ -1,17 +1,49 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 using Prism.Commands;
 using TooDo.Models;
 using System.Data.SQLite;
 using Dapper;
+using System.Linq;
 
 namespace TooDo.ViewModels
 {
-    public class TaskViewModel
+    public class TaskViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public ObservableCollection<Task> Tasks { get; set; }
-        public Task SelectedTask { get; set; }
-        public string NewTaskTitle { get; set; }
+        public ObservableCollection<Task> CompletedTasks { get; set; }
+
+        private Task selectedTask;
+        public Task SelectedTask
+        {
+            get { return selectedTask; }
+            set
+            {
+                if (selectedTask != value)
+                {
+                    selectedTask = value;
+                    RaisePropertyChanged(nameof(SelectedTask));
+                }
+            }
+        }
+
+        private string newTaskTitle;
+        public string NewTaskTitle
+        {
+            get { return newTaskTitle; }
+            set
+            {
+                if (newTaskTitle != value)
+                {
+                    newTaskTitle = value;
+                    RaisePropertyChanged(nameof(NewTaskTitle));
+                }
+            }
+        }
 
         public ICommand AddTaskCommand { get; set; }
         public ICommand CompleteTaskCommand { get; set; }
@@ -27,6 +59,11 @@ namespace TooDo.ViewModels
             AddTaskCommand = new DelegateCommand(AddTask);
             CompleteTaskCommand = new DelegateCommand(CompleteTask);
             DeleteTaskCommand = new DelegateCommand(DeleteTask);
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void InitializeDatabase()
@@ -45,7 +82,9 @@ namespace TooDo.ViewModels
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
-                Tasks = new ObservableCollection<Task>(connection.Query<Task>("SELECT * FROM Tasks"));
+                var tasks = connection.Query<Task>("SELECT * FROM Tasks").ToList();
+                Tasks = new ObservableCollection<Task>(tasks);
+                CompletedTasks = new ObservableCollection<Task>(tasks.Where(t => t.IsCompleted));
             }
         }
 
@@ -74,6 +113,13 @@ namespace TooDo.ViewModels
                     connection.Open();
                     connection.Execute("UPDATE Tasks SET IsCompleted = 1 WHERE Title = @Title", SelectedTask);
                 }
+                RaisePropertyChanged(nameof(SelectedTask));
+
+                if (Tasks.Contains(SelectedTask))
+                {
+                    Tasks.Remove(SelectedTask);
+                    CompletedTasks.Add(SelectedTask);
+                }
             }
         }
 
@@ -86,7 +132,16 @@ namespace TooDo.ViewModels
                     connection.Open();
                     connection.Execute("DELETE FROM Tasks WHERE Title = @Title", SelectedTask);
                 }
-                Tasks.Remove(SelectedTask);
+
+                if (Tasks.Contains(SelectedTask))
+                {
+                    Tasks.Remove(SelectedTask);
+                }
+                else if (CompletedTasks.Contains(SelectedTask))
+                {
+                    CompletedTasks.Remove(SelectedTask);
+                }
+
                 SelectedTask = null;
             }
         }
